2b505137628fd498de4dfc7d44b2d1f3587834cf
RicardoXu <1641137052@qq.com>
2018-09-05 01:03:14 +0800

    commit the common server response

---
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -0,0 +1,33 @@
+package com.mmall.controller.portal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * @ClassName UserController
+ * @Description TODO
+ * @Author root
+ * @Date 18-9-1 下午7:34
+ * @Version 1.0
+ **/
+@Controller
+@RequestMapping("/user/")
+public class UserController {
+
+    /**
+     * login
+     * @param username
+     * @param password
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = "login.do",method = RequestMethod.POST)
+    @ResponseBody
+    public Object login(String username, String password, HttpSession session){
+        return null;
+    }
+}
---
+++ src/main/java/com/mmall/service/impl/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.mmall.service.impl;
+
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+import com.mmall.service.IUserService;
+
+/**
+ * @ClassName UserServiceImpl
+ * @Description TODO
+ * @Author root
+ * @Date 18-9-5 上午1:01
+ * @Version 1.0
+ **/
+public class UserServiceImpl implements IUserService {
+    @Override
+    public ServerResponse<User> login(String username, String password) {
+        return null;
+    }
+}
