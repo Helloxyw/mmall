56ce9fa829780acf404170f06b18e44bb5ebf73e
RicardoXu <1641137052@qq.com>
2018-11-04 23:50:58 +0800

    finish const

---
+++ src/main/java/com/mmall/common/Const.java
@@ -0,0 +1,17 @@
+package com.mmall.common;
+
+/**
+ * @ClassName Const
+ * @Description TODO
+ * @Author root
+ * @Date 18-11-4 下午6:47
+ * @Version 1.0
+ **/
+public class Const {
+    public static final String CURRENT_USER = "currentUser";
+
+    public interface Role {
+        int ROLE_CUSTOMER = 0;//customer
+        int ROLE_ADMIN = 1;//admin
+    }
+}
